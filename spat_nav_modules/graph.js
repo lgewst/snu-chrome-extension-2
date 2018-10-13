dd2ca3ac5cbebd70f3c911f1438ef8072acd319d
HyunSunSHIN <amorphysics@snu.ac.kr>
2018-10-13 19:46:28 +0900

    add make_directed_graph which removes redundancies in edges

--- spat_nav_modules/graph.js
+++ spat_nav_modules/graph.js
@@ -11,7 +11,7 @@
  *
  */
 
-function my_make_data_structure(){
+function make_data_structure(){
     var focusable = document.body.focusableAreas({'mode': 'all'});
     var graph = new Array(focusable.length);
     var dir = ["up", "down", "left", "right"];
@@ -25,4 +25,50 @@ function my_make_data_structure(){
         }
     }
     return graph;
+}
+
+function remove_redundancy_in_array(input_array){
+  var output_array = [];
+  $.each(input_array, function(i, el){
+    if($.inArray(el, output_array) === -1) output_array.push(el);
+});
+  var filtered_output_array = output_array.filter(function (el) {
+  return el != null;
+  });
+  return filtered_output_array;
+}
+
+
+/* A fuction that makes a  directed graph in the form of two dimensional list
+ *
+ * Row
+ * Length of the number of focusble element.
+ *
+ * Column
+ * Nodes | origin | [destinations]
+ *
+ */
+
+ // Here, directed graph means a grpah only having directed edge
+ // that can be represented with a pair of two nodes (from, to)
+ // which is not related to arrow keys direction.
+
+ // To use this function the html code should import JQuery as below.
+ // <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+
+function make_directed_graph(arrow_graph){
+
+  var directed_graph = [];
+
+  for(var i = 0; i < arrow_graph.length; i++){
+    var tmp = new Array();
+    var origin = arrow_graph[i][0];
+    var destinations_with_each_arrow_keys = arrow_graph[i].slice(1,arrow_graph[i].length);
+    var destinations_without_redundancy = remove_redundancy_in_array(destinations_with_each_arrow_keys);
+
+    tmp.push(origin);
+    Array.prototype.push.apply(tmp,destinations_without_redundancy);
+    directed_graph.push(tmp);
+  }
+  return directed_graph;
 }
