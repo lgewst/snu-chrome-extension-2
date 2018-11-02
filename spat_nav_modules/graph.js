490a71fbcb5270e0129a2c8ca91a4f429053ff9e
jisk984 <jisk984@snu.ac.kr>
2018-11-02 14:32:06 +0900

    modify loop detector

--- spat_nav_modules/graph.js
+++ spat_nav_modules/graph.js
@@ -21,23 +21,26 @@ function graph(){
  *
  */
 graph.prototype.make_adj_array = function(direction){
-    var focusable = document.body.focusableAreas({'mode': 'visible'});
+    var focusable = document.body.focusableAreas({'mode': 'all'});
     this.node_num = focusable.length;
     this.visited = new Array(this.node_num).fill(0);
-
     var graph = new Array(this.node_num);
     var dir = ["up", "down", "left", "right"];
 
     for(var i = 0; i < this.node_num; i++){
       focusable[i].node_id = i
-      graph[i] = new Array(5);
-      graph[i][0] = focusable[i];
       if(!direction){
+        graph[i] = new Array(5);
+        graph[i][0] = focusable[i];
         for(var j = 0; j < dir.length; j++){
           graph[i][j+1] = window.__spatialNavigation__.findNextTarget(graph[i][0],dir[j]);
         }
       }
-      else graph[i][direction-1] = window.__spatialNavigation__.findNextTarget(graph[i][0],dir[direction-1]);
+      else {
+        graph[i] = new Array(2);
+        graph[i][0] = focusable[i];
+        graph[i][1] = window.__spatialNavigation__.findNextTarget(graph[i][0],dir[direction-1]);
+      }
     }
     return graph;
 }
@@ -149,7 +152,7 @@ graph.prototype.make_rev_scc = function(scc){
 
   for(var i = 0; i < scc.length; i++){
     for(var j = 1; j < scc[i][0].length; j++){
-      rev_scc[scc[i][0][j]].push(i);
+      rev_scc[scc[i][0][j]][0].push(i);
     }
   }
   return rev_scc;
@@ -175,6 +178,7 @@ graph.prototype.make_scc = function(){
     var node_id = this.stack.pop();
     if(!this.visited[node_id]){
       this.scc.push([]);
+      this.scc[this.scc.length-1].push([]);
       this.rev_dfs(node_id)
     }
   }
@@ -206,6 +210,7 @@ graph.prototype.rev_dfs = function(node_id){
 }
 
 graph.prototype.condensation = function(){
+
   var tmp = [];
   for(var i = 0; i < this.scc.length; i++){
     tmp.length = 0;
@@ -217,13 +222,16 @@ graph.prototype.condensation = function(){
         if(this.adj_list[o_node].scc_id != this.adj_list[node.node_id].scc_id) tmp.push(this.adj_list[o_node].scc_id);
       }
     }
+    var tmp2 = this.scc[i][0];
     $.each(tmp, function(key, value){
-      if($.inArray(value, this.scc[i][0]) === -1) this.scc[i][0].push(value);
+      if($.inArray(value, tmp2) === -1) {
+        tmp2.push(value);
+      }
     })
   }
 }
 
-graph.prototype.trap_visualize = function(border_color){
+graph.prototype.trap_visualizor = function(border_color){
   for(var i = 0; i < this.scc.length; i++){
     if(!this.scc[i][0].length){
       for(var j = 1; j < this.scc[i].length; j++){
@@ -237,10 +245,24 @@ graph.prototype.trap_visualize = function(border_color){
   }
 }
 
-graph.prototype.loop_visualize = function(border_color){
+graph.prototype.loop_visualizor = function(border_color){
   for(var i = 0; i < this.scc.length; i++){
-    if(this.scc[i].length>=2){
-      for(var j = 0; j < this.scc[i].length; j++){
+    if(this.scc[i][0].length>=2){
+      for(var j = 1; j < this.scc[i].length; j++){
+	      this.scc[i][j].style.backgroundColor = "#FDFF47"
+	      this.scc[i][j].style.color = "#47e0ff"
+	      this.scc[i][j].style.borderColor = border_color
+	      this.scc[i][j].style.borderStyle = "dashed"
+	      this.scc[i][j].style.borderWidth = "2"
+      }
+    }
+  }
+}
+
+graph.prototype.unreachable_visualizor = function(border_color){
+  for(var i = 0; i < this.scc.length; i++){
+    if(!this.scc[i][0].length && !this.rev_scc[i][0].length){
+      for(var j = 1; j < this.scc[i].length; j++){
 	      this.scc[i][j].style.backgroundColor = "#FDFF47"
 	      this.scc[i][j].style.color = "#47e0ff"
 	      this.scc[i][j].style.borderColor = border_color
@@ -259,49 +281,60 @@ function trap_detector(){
   graph_trap.adj_list = graph_trap.make_adj_list(graph_trap.adj_array);
   graph_trap.rev_adj_list = graph_trap.make_rev_adj_list(graph_trap.adj_list);
   graph_trap.make_scc();
-  graph_trap.trap_visualize("yellow")
+  graph_trap.trap_visualizor("yellow")
+  console.log(graph_trap.scc);
+  console.log(graph_trap.adj_array);
+  console.log(graph_trap.adj_list);
   return graph_trap.scc
 }
 
 function loop_detector(){
-  var graph_loop = new graph();
-  graph_loop.adj_array_up = graph_loop.make_adj_array(1);
-  graph_loop.adj_list = graph_trap.make_adj_list(graph_trap.adj_array_up);
-  graph_loop.rev_adj_list = graph_trap.make_rev_adj_list(graph_trap.adj_list);
-  graph_loop.make_scc();
-  graph_loop.loop_visualize("red")
+  var graph_loop_up = new graph();
+  graph_loop_up.adj_array = graph_loop_up.make_adj_array(1);
+  graph_loop_up.adj_list = graph_loop_up.make_adj_list(graph_loop_up.adj_array);
+  graph_loop_up.rev_adj_list = graph_loop_up.make_rev_adj_list(graph_loop_up.adj_list);
+  graph_loop_up.make_scc();
+  graph_loop_up.loop_visualizor("red")
 
-  graph_loop.adj_array_down = graph_loop.make_adj_array(2);
-  graph_loop.adj_list = graph_trap.make_adj_list(graph_trap.adj_array_down);
-  graph_loop.rev_adj_list = graph_trap.make_rev_adj_list(graph_trap.adj_list);
-  graph_loop.make_scc();
-  graph_loop.loop_visualize("yellow")
+  var graph_loop_down = new graph();
+  graph_loop_down.adj_array = graph_loop_down.make_adj_array(2);
+  graph_loop_down.adj_list = graph_loop_down.make_adj_list(graph_loop_down.adj_array);
+  graph_loop_down.rev_adj_list = graph_loop_down.make_rev_adj_list(graph_loop_down.adj_list);
+  graph_loop_down.make_scc();
+  graph_loop_down.loop_visualizor("yellow")
 
-  graph_loop.adj_array_left = graph_loop.make_adj_array(3);
-  graph_loop.adj_list = graph_trap.make_adj_list(graph_trap.adj_array_left);
-  graph_loop.rev_adj_list = graph_trap.make_rev_adj_list(graph_trap.adj_list);
-  graph_loop.make_scc();
-  graph_loop.loop_visualize("blue")
+  var graph_loop_left = new graph();
+  graph_loop_left.adj_array = graph_loop_left.make_adj_array(3);
+  graph_loop_left.adj_list = graph_loop_left.make_adj_list(graph_loop_left.adj_array);
+  graph_loop_left.rev_adj_list = graph_loop_left.make_rev_adj_list(graph_loop_left.adj_list);
+  graph_loop_left.make_scc();
+  graph_loop_left.loop_visualizor("blue")
+
   
-  graph_loop.adj_array_right = graph_loop.make_adj_array(4);
-  graph_loop.adj_list = graph_trap.make_adj_list(graph_trap.adj_array_right);
-  graph_loop.rev_adj_list = graph_trap.make_rev_adj_list(graph_trap.adj_list);
-  graph_loop.make_scc();
-  graph_loop.loop_visualize("green")
+  var graph_loop_right = new graph();
+  graph_loop_right.adj_array = graph_loop_right.make_adj_array(4);
+  graph_loop_right.adj_list = graph_loop_right.make_adj_list(graph_loop_right.adj_array);
+  graph_loop_right.rev_adj_list = graph_loop_right.make_rev_adj_list(graph_loop_right.adj_list);
+  graph_loop_right.make_scc();
+  graph_loop_right.loop_visualizor("green");
+  console.log(graph_loop_right.scc);
+  console.log(graph_loop_right.adj_array);
+  console.log(graph_loop_right.adj_list);
 }
 
 function unreachable_detector(){
-  var graph_loop = new graph();
-  graph_unreachable.adj_array = graph_trap.make_adj_array(0);
-  graph_unreachable.adj_list = graph_trap.make_adj_list(graph_trap.adj_array);
-  graph_unreachable.rev_adj_list = graph_trap.make_rev_adj_list(graph_trap.adj_list);
+  var graph_unreachable = new graph();
+  graph_unreachable.adj_array = graph_unreachable.make_adj_array(0);
+  graph_unreachable.adj_list = graph_unreachable.make_adj_list(graph_unreachable.adj_array);
+  graph_unreachable.rev_adj_list = graph_unreachable.make_rev_adj_list(graph_unreachable.adj_list);
   graph_unreachable.make_scc();
   graph_unreachable.make_rev_scc();
+  graph_unreachable.loop_visualizor("purple")
 }
 
 trap_detector();
-loop_detector();
-unreachable_detector();
+// loop_detector();
+// unreachable_detector();
 
 // chrome.storage.sync.set(
 //   {'scc' : trap_detector()}
