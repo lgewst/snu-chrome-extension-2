ad2f31ee646a21939d33daf1b0eb2a58fcf03ae3
HyunsunSHIN <amorphysics@snu.ac.kr>
2018-12-04 15:31:22 +0900

    wired isolation detector and injected detector to manifest

--- spat_nav_modules/isolation_elements_detector.js
+++ spat_nav_modules/isolation_elements_detector.js
@@ -1,4 +1,4 @@
-var res = document.body.focusableAreas({'mode': 'all'});
+var res = isolation_detector()// document.body.focusableAreas({'mode': 'all'});
 var res_arr = Array();
 for(i=0; i<res.length; i++){
 	res_arr[i] = res[i].outerHTML;
