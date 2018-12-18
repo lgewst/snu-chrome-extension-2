95a4386b8add0c3ef0e0e7ddfed85874d169e044
HyunsunSHIN <amorphysics@snu.ac.kr>
2018-12-19 02:00:57 +0900

    hierarchy changed regarding UI

--- detector_js/unreachable_elements_detector.js
+++ detector_js/unreachable_elements_detector.js
@@ -3,8 +3,6 @@ var ret_val = Array();
 var res = unreachable_detector();
 if(res < 0) ret_val = res;
 else{
-	alert(res.length);
-	//var res = document.body.focusableAreas({'mode': 'all'});
 	for(i=0; i<res.length; i++){
 		ret_val[i] = res[i].outerHTML;
 	}
--- devtools.js
+++ devtools.js
@@ -1,7 +1,7 @@
 //This devtools.js will create inspection tool page named as Spat-Nav Accessibility Evaluator
 chrome.devtools.panels.create("Spat-Nav Accessibility Evaluator",
                               "dev.png",
-                              "index.html",
+                              "user_interface_lib/index.html",
                               function(panel) {
 			      });
                               
